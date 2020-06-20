4b652e63663357ccbe3e0cf51220c4d53c98be22
faran4engg <faran4engg@gmail.com>
2020-06-20 17:03:42 +0200

    axios instance updated

--- src/axios/axios-instance.js
+++ src/axios/axios-instance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV !== "production"
-      ? "http://localhost/"
+      ? "https://dev.technotackle.com/farming-company/admin/web/v1"
       : "https://travix-faran-todo.herokuapp.com/",
 });
 
